48c91791bf6667908e5e8f7d4dbdb7c124086ffe
MadnessCod <147450756+MadnessCod@users.noreply.github.com>
2024-07-09 15:52:15 +0330

    create database_creation.py file

---
+++ database_creation.py
@@ -0,0 +1,29 @@
+import peewee
+
+from database_manager import DatabaseManager
+from local_settings import DATABASE
+
+
+database_manager = DatabaseManager(
+    database_name=DATABASE["name"],
+    user=DATABASE["user"],
+    password=DATABASE["password"],
+    host=DATABASE["host"],
+    port=DATABASE["port"],
+)
+
+
+class Proxy(peewee.Model):
+    ip_address = peewee.CharField(max_length=100, verbose_name="ip address")
+    method = peewee.CharField(max_length=100, verbose_name="method")
+    port = peewee.CharField(max_length=100, verbose_name="port")
+    code = peewee.CharField(max_length=100, verbose_name="code")
+    country = peewee.CharField(max_length=100, verbose_name="country")
+    anonymity = peewee.CharField(max_length=100, verbose_name="anonymity")
+    google = peewee.CharField(max_length=100, verbose_name="google")
+    https = peewee.CharField(max_length=100, verbose_name="https")
+    last_checked = peewee.CharField(max_length=100, verbose_name="last checked")
+    status = peewee.BooleanField(default=False, verbose_name="status")
+
+    class Meta:
+        database = database_manager.db
