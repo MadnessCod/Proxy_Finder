950ae60ebff447c3d6f0b028deaec80ba8cffdad
unknown <sadrahosseini98@gmail.com>
2024-08-05 18:55:44 +0330

    changed .gitignore file

--- Scrapper/celery.py
+++ Scrapper/celery.py
@@ -4,7 +4,6 @@ from celery.schedules import crontab
 from local_settings import BROKER_URL, BACKEND_URL
 
 
-print(BROKER_URL, BACKEND_URL)
 app = Celery('ProxyFinder', broker=BROKER_URL, backend=BACKEND_URL)
 
 app.autodiscover_tasks(['Scrapper'])
@@ -14,10 +13,10 @@ app.conf.broker_connection_retry = True
 app.conf.beat_schedule = {
     "scraping": {
         "task": "Scrapper.tasks.main",
-        "schedule": crontab('*/10'),
+        "schedule": crontab('*/15'),
     },
     "evaluator": {
         "task": "Scrapper.tasks.proxy_evaluator_main",
-        "schedule": crontab('*/5'),
+        "schedule": crontab('*/10'),
     },
 }
